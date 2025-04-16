d77c952f860619085e176cedb06810c1553f0b99
Optimus Elikem <portiaadaletey@gmail.com>
2025-04-16 11:49:41 +0000

    hamburger menu worked on

--- JS/script.js
+++ JS/script.js
@@ -5,6 +5,32 @@
 //     };
 // });
 
+const menuBtn = document.getElementById("menu-btn");
+const navbar = document.querySelector(".navbar");
+
+// Toggle menu
+menuBtn.addEventListener("click", () => {
+  menuBtn.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+});
+
+// Close when a nav link is clicked
+document.querySelectorAll('.navbar a').forEach(link => {
+  link.addEventListener("click", () => {
+    navbar.classList.remove("active");
+    menuBtn.classList.remove("fa-times");
+  });
+});
+
+// Close when clicking outside the navbar
+document.addEventListener("click", (e) => {
+  if (!navbar.contains(e.target) && !menuBtn.contains(e.target)) {
+    navbar.classList.remove("active");
+    menuBtn.classList.remove("fa-times");
+  }
+});
+
+
 var swiper = new Swiper('.review-slider', {
     spaceBetween: 20,
     pagination: {
